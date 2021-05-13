d411c3200aa75f5d1f9870b3c8f763bf7ea6b3e4
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-13 19:22:11 -0400

    Header cmpnt added

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-center">Employee Directory</h1>
+            <p className="text-center">
+                #Click on the columns below to filter or use search for precise results.#
+            </p>
+        </header>
+    );
+    
+};
+
+export default Header;
